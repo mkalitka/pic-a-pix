3955d5613877b9b4cde7dee807570f19a23261f6
NoratanS <kbielski23@gmail.com>
2023-01-20 19:30:39 +0100

    k4sia creates solution
    Brought back the "create matrix" function and renamed it to "create solution", also translated the comments

--- pic_a_pix/k4sia-image.py
+++ pic_a_pix/k4sia-image.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 # converting the image
 def convert_img(img_name, size, threshold):
-    """WCZYTANIE ZDJECIA, ZAMIANA NA SZARY, PRZESKALOWANIE"""
+    """Loading the image and converting it"""
     with Image.open(img_name) as img_a:
         width, height = img_a.size
         n_2, m_2 = w_h(width, height, size)
@@ -13,12 +13,29 @@ def convert_img(img_name, size, threshold):
     return img_a
 
 
+# creating the solution
+def create_solution(img):
+    """Creating a solution array"""
+    n = img.height
+    m = img.width
+    pixels = [[img.getpixel((i, j)) for i in range(m)] for j in range(n)]
+
+    # replacing pixel values in matrix: 1 - black, 0 - white
+    for i in range(n):
+        for j in range(m):
+            if pixels[i][j] == 255:
+                pixels[i][j] = 0
+            else:
+                pixels[i][j] = 1
+    return pixels
+
+
 def columns_and_rows(img):
     n_h = img.height
     m_w = img.width
     pixels = [[img.getpixel((i, j)) for i in range(m_w)] for j in range(n_h)]
-    rows, cols = [[]for i in range(n_h)], [[]for i in range(m_w)]
-    """ ZLICZANIE CIAGOW CZARNYCH PIKSELI PO WIERSZACH """
+    rows, cols = [[] for i in range(n_h)], [[] for i in range(m_w)]
+    """Counting blocks of black pixels by rows"""
     for i in range(n_h):
         sum_pix = 0
         for j in range(m_w):
@@ -29,7 +46,7 @@ def columns_and_rows(img):
                 sum_pix += 1
         if sum_pix != 0:
             rows[i].append(sum_pix)
-    """ ZLICZANIE CIAGOW CZARNYCH PIKSELI PO KOLUMNACH """
+    """Counting blocks of black pixels by columns"""
     for i in range(m_w):
         sum_pix = 0
         for j in range(n_h):
@@ -45,21 +62,21 @@ def columns_and_rows(img):
 
 
 def save_nonotuple_to_file(img, r_and_c, file):
-    """PRZEPISANIE LICZBY CZARNYCH KWADRATOW Z KOLUMN I WIERSZY DO PLIKU"""
+    """Saving a number of black pixels from rows and columns to a file"""
     nono = open(file, "w")
     nono.write(f"{img.height} {img.width}\n")
     for r_c in r_and_c:
-         for line in r_c:
-             if len(line) == 0:
-                 nono.write("0")
-             for numb in line:
-                 nono.write(f"{numb} ")
-             nono.write("\n")
+        for line in r_c:
+            if len(line) == 0:
+                nono.write("0")
+            for numb in line:
+                nono.write(f"{numb} ")
+            nono.write("\n")
     nono.close()
 
 
 def div_five(a_1):
-    """ZWRACA LICZBE ZAOKRAGLONA DO 5"""
+    """Returns a number rounded to 5"""
     if a_1 % 5 <= 2:
         a_1 = a_1 - (a_1 % 5)
     else:
@@ -68,7 +85,7 @@ def div_five(a_1):
 
 
 def w_h(width, height, size):
-    """PROPORCJONALNE SKALOWANIE ZDJECIA"""
+    """Scaling the picture"""
     print("Dimensions:", width, height)
     if width < height:
         m_1 = size
@@ -79,7 +96,7 @@ def w_h(width, height, size):
         n_1 = size
         m_1 = int((n_1 * width) / height)
         m_1 = 5 * round(m_1 / 5)
-    print("wymiary", m_1, n_1)
+    print("Board Dimensions", m_1, n_1)
     return m_1, n_1
 
 
@@ -89,3 +106,4 @@ converted = convert_img(IMAGE_NAME, IMG_SIZE, 160)
 converted.show()
 nonotuple = columns_and_rows(converted)
 save_nonotuple_to_file(converted, nonotuple, "nono_board.txt")
+solution = create_solution(converted)
