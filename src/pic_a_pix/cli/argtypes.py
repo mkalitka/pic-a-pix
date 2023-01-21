b7308efd7c2bd6945612e883f38d4ba452cc6a4a
mkalitka <mikolaj.kalitka@proton.me>
2023-01-21 03:36:17 +0100

    Restructured project

---
+++ src/pic_a_pix/cli/argtypes.py
@@ -0,0 +1,37 @@
+import os
+import argparse
+
+from PIL import Image
+
+
+def img_path(path: str) -> str:
+    """Check if string is a valid path to an image"""
+    if os.path.isfile(path):
+        try:
+            with Image.open(path):
+                return path
+        except IOError as exc:
+            raise argparse.ArgumentTypeError(
+                f"{path} is not a valid image path"
+            ) from exc
+    raise argparse.ArgumentTypeError(f"{path} is not a valid image path")
+
+
+def size(img_size: str) -> int:
+    """Check if size is valid"""
+    try:
+        if 5 <= int(img_size) <= 100:
+            return int(img_size)
+        raise argparse.ArgumentTypeError("invalid image size (5-100)")
+    except ValueError as exc:
+        raise argparse.ArgumentTypeError("invalid image size (5-100)") from exc
+
+
+def threshold(img_threshold: str) -> int:
+    """Check if threshold is valid"""
+    try:
+        if 0 <= int(img_threshold) <= 255:
+            return int(img_threshold)
+        raise argparse.ArgumentTypeError("invalid threshold (0-255)")
+    except ValueError as exc:
+        raise argparse.ArgumentTypeError("invalid threshold (0-255)") from exc
--- src/pic_a_pix/k4sia_image.py
+++ src/pic_a_pix/k4sia_image.py
@@ -1,8 +1,9 @@
+from typing import List, Tuple
+
 from PIL import Image
 
 
-# converting the image
-def convert_img(img_name, size, threshold):
+def convert_img(img_name: str, size: int, threshold: int) -> Image.Image:
     """Loading the image and converting it"""
     with Image.open(img_name) as img_a:
         width, height = img_a.size
@@ -13,16 +14,15 @@ def convert_img(img_name, size, threshold):
     return img_a
 
 
-# creating the solution
-def create_solution(img):
+def create_solution(img: Image.Image) -> List[List[int]]:
     """Creating a solution array"""
-    n = img.height
-    m = img.width
-    pixels = [[img.getpixel((i, j)) for i in range(m)] for j in range(n)]
+    height = img.height
+    width = img.width
+    pixels = [[img.getpixel((i, j)) for i in range(width)] for j in range(height)]
 
-    # replacing pixel values in matrix: 1 - black, 0 - white
-    for i in range(n):
-        for j in range(m):
+    # Replacing pixel values in matrix: 1 - black, 0 - white
+    for i in range(height):
+        for j in range(width):
             if pixels[i][j] == 255:
                 pixels[i][j] = 0
             else:
@@ -30,12 +30,14 @@ def create_solution(img):
     return pixels
 
 
-def columns_and_rows(img):
+def columns_and_rows(img: Image.Image) -> Tuple[List[List[int]], List[List[int]]]:
+    """Counts black and white pixels and returns them as rows and columns"""
     n_h = img.height
     m_w = img.width
     pixels = [[img.getpixel((i, j)) for i in range(m_w)] for j in range(n_h)]
-    rows, cols = [[] for i in range(n_h)], [[] for i in range(m_w)]
-    """Counting blocks of black pixels by rows"""
+    rows, cols = [[] for _ in range(n_h)], [[] for _ in range(m_w)]
+
+    # Counting blocks of black pixels by rows
     for i in range(n_h):
         sum_pix = 0
         for j in range(m_w):
@@ -46,7 +48,8 @@ def columns_and_rows(img):
                 sum_pix += 1
         if sum_pix != 0:
             rows[i].append(sum_pix)
-    """Counting blocks of black pixels by columns"""
+
+    # Counting blocks of black pixels by columns
     for i in range(m_w):
         sum_pix = 0
         for j in range(n_h):
@@ -57,53 +60,35 @@ def columns_and_rows(img):
                 sum_pix += 1
         if sum_pix != 0:
             cols[i].append(sum_pix)
+
     r_and_c = (rows, cols)
+
     return r_and_c
 
 
-def save_nonotuple_to_file(img, r_and_c, file):
-    """Saving a number of black pixels from rows and columns to a file"""
-    nono = open(file, "w")
-    nono.write(f"{img.height} {img.width}\n")
-    for r_c in r_and_c:
-        for line in r_c:
-            if len(line) == 0:
-                nono.write("0")
-            for numb in line:
-                nono.write(f"{numb} ")
-            nono.write("\n")
-    nono.close()
-
-
-def div_five(a_1):
+def div_five(a_1: int) -> int:
     """Returns a number rounded to 5"""
     if a_1 % 5 <= 2:
         a_1 = a_1 - (a_1 % 5)
     else:
         a_1 = a_1 + (5 - (a_1 % 5))
+
     return a_1
 
 
-def w_h(width, height, size):
+def w_h(width: int, height: int, size: int) -> Tuple[int, int]:
     """Scaling the picture"""
     print("Dimensions:", width, height)
+
     if width < height:
         m_1 = size
         n_1 = int((m_1 * height) / width)
         n_1 = 5 * round(n_1 / 5)
-
     else:
         n_1 = size
         m_1 = int((n_1 * width) / height)
         m_1 = 5 * round(m_1 / 5)
+
     print("Board Dimensions", m_1, n_1)
+
     return m_1, n_1
-
-
-IMG_SIZE = 20
-IMAGE_NAME = "palma.png"
-converted = convert_img(IMAGE_NAME, IMG_SIZE, 160)
-converted.show()
-nonotuple = columns_and_rows(converted)
-save_nonotuple_to_file(converted, nonotuple, "nono_board.txt")
-solution = create_solution(converted)
