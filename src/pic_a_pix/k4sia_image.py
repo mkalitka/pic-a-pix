a71a1227002998eeed2cc64dd2fcc2da92276a2e
Sztosik <trzos.katarzyna1305@gmail.com>
2023-01-24 00:09:10 +0100

    board function

--- src/pic_a_pix/k4sia_image.py
+++ src/pic_a_pix/k4sia_image.py
@@ -1,20 +1,18 @@
 from typing import List, Tuple
-
 from PIL import Image
 
-
-def convert_img(img_name: str, size: int, threshold: int) -> Image.Image:
+def convert_img(img_name: str, lvl: int, threshold: int) -> Image.Image:
     """Loading the image and converting it"""
     with Image.open(img_name) as img_a:
         width, height = img_a.size
-        n_2, m_2 = w_h(width, height, size)
+        n_2, m_2 = w_h(width, height, lvl)
         img_a = img_a.convert("L")
         img_a = img_a.resize((n_2, m_2))
         img_a = img_a.point(lambda p: 255 if p > threshold else 0)
     return img_a
 
 
-def create_solution(img: Image.Image) -> List[List[int]]:
+def create_matrix(img: Image.Image) -> List[List[int]]:
     """Creating a solution array"""
     height = img.height
     width = img.width
@@ -27,13 +25,23 @@ def create_solution(img: Image.Image) -> List[List[int]]:
                 pixels[i][j] = 0
             else:
                 pixels[i][j] = 1
+
     return pixels
+
+def save_matrix_to_file(pixels, file):
+    matrix = open(file, 'w')
+    for line in pixels:
+        for i in line:
+            matrix.write(str(i))
+        matrix.write('\n')
+    matrix.close()
 
 
 def columns_and_rows(img: Image.Image) -> Tuple[List[List[int]], List[List[int]]]:
     """Counts black and white pixels and returns them as rows and columns"""
     n_h = img.height
     m_w = img.width
+
     pixels = [[img.getpixel((i, j)) for i in range(m_w)] for j in range(n_h)]
     rows, cols = [[] for _ in range(n_h)], [[] for _ in range(m_w)]
 
@@ -62,33 +70,42 @@ def columns_and_rows(img: Image.Image) -> Tuple[List[List[int]], List[List[int]]
             cols[i].append(sum_pix)
 
     r_and_c = (rows, cols)
-
     return r_and_c
 
-
-def div_five(a_1: int) -> int:
-    """Returns a number rounded to 5"""
-    if a_1 % 5 <= 2:
-        a_1 = a_1 - (a_1 % 5)
-    else:
-        a_1 = a_1 + (5 - (a_1 % 5))
-
-    return a_1
-
-
-def w_h(width: int, height: int, size: int) -> Tuple[int, int]:
+def w_h(width: int, height: int, lvl: int) -> Tuple[int, int]:
     """Scaling the picture"""
     print("Dimensions:", width, height)
+    if lvl == 0:   # easy
+        size_w = 10
+        size_h = 20
+    if lvl == 1:   # medium
+        size_w = 15
+        size_h = 30
+    if lvl == 2:   # hard
+        size_w = 20
+        size_h = 40
 
     if width < height:
-        m_1 = size
-        n_1 = int((m_1 * height) / width)
-        n_1 = 5 * round(n_1 / 5)
-    else:
-        n_1 = size
-        m_1 = int((n_1 * width) / height)
+        n_1 = size_h
+        m_1 = int((n_1 * width) / height )
         m_1 = 5 * round(m_1 / 5)
+    else:
+        m_1 = size_w
+        n_1 = int((m_1 * width) / height)
+        n_1 = 5 * round(n_1 / 5)
+        m_1, n_1 = n_1, m_1
 
     print("Board Dimensions", m_1, n_1)
 
     return m_1, n_1
+
+
+######## funkcje ktore maja byc w glownym miejscu i co beda robic
+# im = 'wiel.png' #jakas funkcja cowczytuje zdjecie
+# lvl=0
+# img = convert_img(im, lvl, 160)  #img to zdjecie male szare kwadraty .jpg
+# matrix = create_matrix(img)  # robi tablice dwuwymiarowa z 0 1 pozniej bedzie uzyta w funkcji check_if_correcti cr1sie
+# # save_matrix_to_file(matrix, 'matrix.txt')  #to na zapisuje tablice 0 1 do pliku matrix.txt (na razie nie potrzebne)
+# left_num, up_num = columns_and_rows(img) #dwie tablice dwuwymiarowe z liczbami ktore maja byc po lewej i na gorze planszy (podawane do create_board w cr1is/gui)
+# print(left_num)
+# print(up_num)
