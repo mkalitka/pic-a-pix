fac726264db426071e164efd86e94a4271d8afec
Floper <43756755+Flop3r@users.noreply.github.com>
2023-01-24 00:19:24 +0100

    BugFix: open_file() did not show any files in dialog window. (#38)
    * BugFix: file_open() did not show any files in dialog window. (#37)
    
    * BugFix: open_file() did not show any files in dialog window.

--- src/pic_a_pix/gui/functions.py
+++ src/pic_a_pix/gui/functions.py
@@ -1,7 +1,9 @@
 from PIL import ImageColor
 from tkinter import filedialog
 
-IMG_EXTS = r"*.png; *.jpg"
+IMG_EXTS = r"*.png *.jpg *.jpeg *.jfif *.pjpeg *.pjp *.webp"
+IMG_JPEG = r"*.jpg *.jpeg *.jfif *.pjpeg *.pjp"
+
 
 # Changing color
 def hex_to_rgb(hex_color):
@@ -25,12 +27,14 @@ def check_solution(correct_arr, to_check_arr):
     return 1
 
 #Get file's path
-def file_open():
+def open_file():
     path = filedialog.askopenfile(title = "Select a File",
-                                  filetypes=(".png, .jpg", IMG_EXTS))
+                                  filetypes=[("All files", IMG_EXTS),
+                                             ("JPEG files", IMG_EXTS),
+                                             ("PNG files", "*.png")])
 
     return path
 
-#Test file_open function
-# p = file_open()
+#Test open_file function
+# p = open_file()
 # (k4sia_image.convert_img(p.name, 1, 90)).show()
