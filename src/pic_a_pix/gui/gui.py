3ff4c6062afe512346eb8033102dc0773fd0e4e7
MikolKar <mikolaj.karapka@gmail.com>
2023-01-23 15:00:05 +0100

    Gui v2

--- src/pic_a_pix/gui/gui.py
+++ src/pic_a_pix/gui/gui.py
@@ -1,5 +1,6 @@
-import pygame, functions
-
+import pygame
+from pic_a_pix import k4sia_image
+from pic_a_pix.gui import functions
 max_tps = 60.0
 
 WIDE, HIGH = 1080, 720
@@ -9,62 +10,47 @@ display_color = functions.hex_to_rgb("#263238")
 pygame.display.set_caption("Pic-a-Pix")
 
 clock = pygame.time.Clock()
-TPImage = pygame.image.load(
-    "/home/mikol/Downloads/324977038_699397155072774_590940961041445160_n.png"
-).convert()
-
-
 class BUTTON:
-    def __init__(self, color, position):
-        self.color = functions.hex_to_rgb(color)
-        self.position = position
-
-    def BOX(self):
-        obj_lenght = HIGH * 0.32
-        heigh_obj = HIGH * 0.5 - obj_lenght / 2
-        width_obj = WIDE * self.position - obj_lenght / 2
-        box = pygame.Rect(width_obj, heigh_obj, obj_lenght, obj_lenght)
-        pygame.draw.rect(screen, self.color, box)
-        return box
-
-
-# class Window:
-#     def __int__(self, win):
-#         self.win = win
-#     def
+    def __init__(self, path, x, y, scale):
+        self.image = pygame.image.load(path).convert_alpha()
+        self.width = self.image.get_width() * scale
+        self.heigh = self.image.get_height() * scale
+        self.image = pygame.transform.scale(self.image, (int(self.width), int(self.heigh)))
+        self.cliked = False
+        self.x = x * WIDE - self.width/2
+        self.y = y * HIGH - self.heigh/2
+        self.rect = pygame.Rect(self.x, self.y,self.width,self.heigh)
+    def draw_menu(self):
+        action = False
+        if self.cliked == False:
+            screen.blit(self.image, self.rect)
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            if self.cliked == True:
+                action = True
+        return action
 def main():
-    global box, display_color
+    global display_color
     run = True
     delta = 0.0
     clicked = False
-    in_pos_1, in_pos_2, in_pos_3 = False, False, False
     while run:
         # Initialization
         pygame.init()
-
         # Drawing
         screen.fill(display_color)
         # Drawing object
-        if clicked == False:
-            # BOX_1
-            box = BUTTON("#1261B0", 0.5).BOX()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                if functions.check_if_clicked(mouse_pos, box):
-                    clicked = True
+                action_1 = BUTTON()
 
-        # image
-        # screen.blit(TPImage, (100, 200))
         # Ticking
         delta += clock.tick() / 1000.0
         while delta > 1 / max_tps:
             delta -= 1 / max_tps
         pygame.display.flip()
     pygame.quit()
-
-
 if __name__ == "__main__":
     main()
